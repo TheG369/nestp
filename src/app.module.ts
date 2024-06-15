25c875e07cf8af4a7559b93ec2deae43d9515c45
Giovani <94003261+TheG369@users.noreply.github.com>
2024-06-15 14:28:33 -0600

    Revert "Implementar direccion"

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { DireccionModule } from './direccion/direccion.module';
 
 @Module({
-  imports: [DireccionModule],
+  imports: [],
   controllers: [AppController],
   providers: [AppService],
 })
