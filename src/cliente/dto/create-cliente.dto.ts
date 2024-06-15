cfa1f0365ed431c30b5ee6a3eb867ac84644dff5
Giovani <94003261+TheG369@users.noreply.github.com>
2024-06-15 15:00:49 -0600

    Update create-cliente.dto.ts

--- src/cliente/dto/create-cliente.dto.ts
+++ src/cliente/dto/create-cliente.dto.ts
@@ -1,1 +1,15 @@
-export class CreateClienteDto {}
+export class CreateClienteDto {
+   nombre: string;
+   apellidos: string;
+   rfc: string;
+   email: string;
+   telefono: string;
+   estatus?: boolean;
+    direccion: {
+    calle: string;
+    numeroExt: string;
+    numeroInt: string;
+    codigoPostal: string;
+    localidadId: number;
+  };
+}
