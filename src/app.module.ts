83c85cbe2471d01c70daaa874999ac9adc290074
Giovani <94003261+TheG369@users.noreply.github.com>
2024-06-15 14:45:44 -0600

    Update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { EstadosModule } from './estado/estado.module';
+import { EstadoModule } from './estado/estado.module';
 import { PrismaService } from './prisma/prisma.service';
-import { MunicipiosModule } from './municipio/municipio.module';
-import { LocalidadesModule } from './localidad/localidad.module';
-import { ClientesModule } from './cliente/cliente.module';
-import { DireccionesModule } from './direccion/direccion.module';
+import { MunicipioModule } from './municipio/municipio.module';
+import { LocalidadModule } from './localidad/localidad.module';
+import { ClienteModule } from './cliente/cliente.module';
+import { DireccionModule } from './direccion/direccion.module';
 
 @Module({
   imports: [EstadoModule, MunicipioModule, LocalidadModule, ClienteModule, DireccionModule],
